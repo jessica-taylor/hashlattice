3deb849a76829750bbaee31729ffb98f7634f7c8
Jessica Taylor <jacob@jacob-shiny.(none)>
2014-04-08 16:57:52 -0700

    fixed equality code

--- test/testValue.js
+++ test/testValue.js
@@ -1,5 +1,7 @@
-var Value = require('../lib/value');
 var assert = require('assert');
+var util = require('util');
+
+var Value = require('../lib/value');
 
 var testDataValues = [
   10,
@@ -9,19 +11,23 @@ var testDataValues = [
   'hello',
   new Buffer('af4532', 'hex'),
   [],
+  [1, 2],
   [1, 2, 3],
+  [1, 2, 4],
   [true, 2, [false, 'hi', new Buffer('abc5f7', 'hex')]],
   {},
   {'a': 1, 'b': 2},
+  {'a': 2, 'b': 2},
   {'a': true, 'b': 'hi', 'c': [true, 7, {'x': new Buffer('fede', 'hex')}]}
 ];
 
 describe('value', function() {
   describe('valuesEqual', function() {
     for (var i = 0; i < testDataValues.length; i++) {
-      for (var j = 0; j < testDataValues.length; i++) {
+      for (var j = 0; j < testDataValues.length; j++) {
         assert.equal(i == j,
-                     Value.valuesEqual(testDataValues[i], testDataValues[j]));
+                     Value.valuesEqual(testDataValues[i], testDataValues[j]),
+                     util.inspect([testDataValues[i], testDataValues[j]]));
       }
     }
   });
