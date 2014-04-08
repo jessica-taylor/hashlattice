fc41d12ec23afed95c46382b5991e2cd3dc35640
Jessica Taylor <jacob@jacob-shiny.(none)>
2014-04-08 16:33:06 -0700

    values equal

--- src/value/index.js
+++ src/value/index.js
@@ -4,6 +4,7 @@
 
 var Crypto = require('crypto');
 var assert = require('better-assert');
+var _ = require('underscore');
 
 var Types = require('./types');
 var Encoding = require('./encoding'),
@@ -32,6 +33,34 @@ function isData(data) {
     default:
       return false;
   }
+}
+
+function valuesEqual(v1, v2) {
+  var t1 = Types.valueType(v1), t2 = Types.valueType(t2);
+  if (t1 != t2) return false;
+  switch (t1) {
+    case 'null':
+    case 'boolean':
+    case 'double':
+    case 'string':
+      return v1 == v2;
+    case 'buffer':
+      return v1.toString() == v2.toString();
+    case 'array':
+      return v1.length == v2.length &&
+        _.every(_.zip(v1, v2), function(x1x2) {
+          return valuesEqual(x1[0], x2[0]);
+        });
+    case 'dict':
+      var keys1 = _.keys(t1);
+      var keys2 = _.keys(t2);
+      keys1.sort();
+      keys2.sort();
+      return keys1 == keys2 && _.every(keys1, function(key) {
+        return valuesEqual(v1[key], v2[key]);
+      });
+  }
+  return false;
 }
 
 function hashBytes(buf) {
