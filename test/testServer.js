94fa567093e1838748d976ca919699a151327958
Jessica Taylor <jessica.liu.taylor@gmail.com>
2014-04-09 23:10:27 -0700

    sandbox tests

--- test/testServer.js
+++ test/testServer.js
@@ -5,25 +5,7 @@ var _ = require('underscore');
 var Value = require('../lib/value');
 var Cache = require('../lib/cache');
 var Server = require('../lib/server').Server;
-
-var testDataValues = [
-  10,
-  null,
-  true,
-  false,
-  'he',
-  'hello',
-  new Buffer('af4532', 'hex'),
-  [],
-  [1, 2],
-  [1, 2, 3],
-  [1, 2, 4],
-  [true, 2, [false, 'hi', new Buffer('abc5f7', 'hex')]],
-  {},
-  {'a': 1, 'b': 2},
-  {'a': 2, 'b': 2},
-  {'a': true, 'b': 'hi', 'c': [true, 7, {'x': new Buffer('fede', 'hex')}]}
-];
+var testDataValues = require('./testingUtil').testDataValues;
 
 describe('Server', function() {
   describe('getHashData', function() {
