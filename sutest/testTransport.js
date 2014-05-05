5c95a9f5b3886fce48189a9c1074ecfe2141c61a
Jessica Taylor <jessica.liu.taylor@gmail.com>
2014-05-04 20:13:07 -0700

    it passes (kinda)

--- sutest/testTransport.js
+++ sutest/testTransport.js
@@ -11,26 +11,21 @@ var Mininet = require('../lib/mininet');
 describe('Transport', function() {
   describe('request', function() {
     var mn = new Mininet('linear,2');
+    after(function() {
+      mn.close();
+    });
     it('should send messages correctly', function (done) {
       this.timeout(5000);
       mn.getIP(2, function(err, destIP) {
         if (err) {
-          mn.close();
           done(err);
         } else {
-          mn.getPort(2, function(err, destPort) {
-            if (err) {
-              mn.close();
-              done('Error while retrieving destination port');
-            }
-            var sender = mn.runCommand(1, 'node ' + Path.join(__dirname, 'transportTestSend') + ' ' + destIP + ' ' + destPort);
-            var receiver = mn.runCommand(2, 'node ' + Path.join(__dirname, 'transportTestReceive'));
-            Utilities.readAllFromStream(sender, function(err, data) {
-              assert(!err, err);
-              assert.equal('done\n', data.toString('utf8'));
-              mn.close();
-              done();
-            });
+          var sender = mn.runCommand(1, 'node ' + Path.join(__dirname, 'transportTestSend') + ' ' + destIP + ' ' + 13337);
+          var receiver = mn.runCommand(2, 'node ' + Path.join(__dirname, 'transportTestReceive'));
+          Utilities.readAllFromStream(sender, function(err, data) {
+            assert(!err, err);
+            assert.equal('done\n', data.toString('utf8'));
+            done();
           });
         }
       })
--- sutest/transportTestReceive.js
+++ sutest/transportTestReceive.js
@@ -5,16 +5,13 @@ var Transport = require('../lib/network/transport');
 
 var udpTransport = new Transport.UdpTransport();
 
-console.warn('receiver starting');
 udpTransport.startServer(
   function(reqObj, cb) {
-    console.log('Hark, a request object!');
     assert.equal('hi there', reqObj);
     cb(null, 'hello to you too');
-    // cb();
   },
   function (err, data) {
-console.warn('receiver started!');
     assert(!err, err);
   });
-// TODO : how do we confirm that this message was actually sent and received?
+
+setTimeout(process.exit, 2000);
