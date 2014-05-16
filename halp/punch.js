30a5f1dd950daa0428d177eba5b0f040d636afe2
Jessica Taylor <jessica.liu.taylor@gmail.com>
2014-05-15 19:25:04 -0700

    halp punch

---
+++ halp/punch.js
@@ -0,0 +1,19 @@
+var Readline = require('readline');
+var Dgram = require('dgram');
+
+var ip = process.argv[2];
+var port = 13337;
+
+var socket = Dgram.createSocket('udp4', function(msg) {
+  console.log('got ' + msg.toString('utf8'));
+});
+socket.bind(port, function() {
+  console.log('bound');
+  function sendMessageToOther() {
+    console.log('sending');
+    var buf = new Buffer('hello', 'utf8');
+    socket.send(buf, 0, buf.length, port, ip);
+    setTimeout(sendMessageToOther, 1000);
+  }
+  sendMessageToOther();
+});
