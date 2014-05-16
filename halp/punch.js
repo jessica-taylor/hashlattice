f816c8b3d96f4fbc807ea5273a3036d37355d1a1
Jessica Taylor <jessica.liu.taylor@gmail.com>
2014-05-15 19:53:28 -0700

    punch options

--- halp/punch.js
+++ halp/punch.js
@@ -1,12 +1,17 @@
 var Readline = require('readline');
 var Dgram = require('dgram');
 
-var ip = process.argv[2];
-var port = 13338;
+var argv = require('optimist').argv;
+console.log(argv);
+
+var ip = argv._[0];
+var port = 13337 || argv.port;
+var nosend = argv.nosend || false;
 
 var socket = Dgram.createSocket('udp4', function(msg) {
   console.log('got ' + msg.toString('utf8'));
 });
+
 socket.bind(port, function() {
   console.log('bound');
   function sendMessageToOther() {
@@ -15,7 +20,7 @@ socket.bind(port, function() {
     socket.send(buf, 0, buf.length, port, ip);
     setTimeout(sendMessageToOther, 1000);
   }
-  if (process.argv[3] !== 'nosend') {
+  if (!nosend) {
     sendMessageToOther();
   }
 });
