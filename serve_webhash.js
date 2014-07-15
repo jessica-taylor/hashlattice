132fe49ffb7f3c7b9a0bb6e515460630f4761a6f
Jessica Taylor <jessica.liu.taylor@gmail.com>
2014-07-14 22:34:52 -0700

    updated serve_webhash

--- serve_webhash.js
+++ serve_webhash.js
@@ -17,7 +17,7 @@ var port = argv.p || 1337;
 
 var staticContent = ['webhash.js', 'weblib.js'];
 
-ChildProcess.exec('node_modules/.bin/browserify ./lib/weblib.js -o webhash/weblib.js && node_modules/.bin/browserify ./lib/webhash.js -o webhash/webhash.js', function(err, stdout, stderr) {
+ChildProcess.exec('./compile && node_modules/.bin/browserify ./lib/weblib.js -o webhash/weblib.js && node_modules/.bin/browserify ./lib/webhash.js -o webhash/webhash.js && ./clean', function(err, stdout, stderr) {
   if (stdout || stderr) {
     console.log('stdout:', stdout);
     console.log('stderr:', stderr);
